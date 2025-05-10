5f78919ea1be5b37b8c801ce51bc7992271971a6
Nathan <ndowens@artixlinux.org>
2025-05-10 00:45:11 -0500

    add

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -5,3 +5,5 @@ alias am="artix-metro"
 export SKIPGPGPASSPROMPT=true
 export SSHKEYSIGN="$HOME/.ssh/id_rsa"
 fish_ssh_agent
+
+export PATH="$HOME/.local/bin:$HOME/.local/sbin:$HOME/.local/share/applications:/home/linuxbrew/.linuxbrew/bin:$PATH"
