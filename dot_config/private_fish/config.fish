1f6f896a096e6ba03e0fd76e3b05ee5819a1bc25
Nathan <ndowens@artixlinux.org>
2025-07-27 03:01:24 -0500

    finishing touch for ssh-agent

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -136,4 +136,6 @@ if [ -z "$SSH_AUTH_SOCK" ]; then
     eval $(ssh-agent -s)
 end
 
-ssh-add -l > /dev/null 2>&1 || ssh-add ~/.ssh/private_key`
+#ssh-add -l > /dev/null 2>&1 || ssh-add ~/.ssh/private_key
+cat ~/.ssh/private_key | ssh-add  2&>1 /dev/null
+
